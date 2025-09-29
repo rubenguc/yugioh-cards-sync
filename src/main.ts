c23dc4924cead2562963175f0bae3e3713d12077
Ruben <rubdeivis@gmail.com>
2025-09-29 11:29:32 -0400

    change to fastify + config for env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { envs } from './config/env';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('main');
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen(envs.port, '0.0.0.0');
+
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
